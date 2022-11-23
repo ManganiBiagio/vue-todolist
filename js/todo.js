ef8e34b8a289b098f8d62af970edb06621ce6ac9
ManganiBiagio <biagiomangani@live.it>
2022-11-23 15:46:20 +0100

    add new task

--- js/todo.js
+++ js/todo.js
@@ -14,9 +14,29 @@ createApp({
             new ToDoItem("prova3",false),
             new ToDoItem("prova4",false),
 
-        ]
+        ],
+        newTask:{
+            text:""
+        }
 
     };
+  },
+  methods:{
+    onTaskClick(iClickedTask){
+        const taskClicked=this.toDoList[iClickedTask];
+        taskClicked.done=!taskClicked.done;
+        
+    },
+    onDeleteTask(iDeleteTask){
+        this.toDoList.splice(iDeleteTask,1);
+        
+
+    },
+    taskSubmit(){
+        const newTask=new ToDoItem(this.newTask.text,false);
+        this.toDoList.push(newTask)
+    }
+
   },
   mounted(){
     
